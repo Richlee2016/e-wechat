521813a63b159a803599b39008d244596268942f
Richlee2016 <345642459@qq.com>
2019-02-19 10:12:41 +0800

    2-19

--- app/service/Book.ts
+++ app/service/Book.ts
@@ -1,17 +1,20 @@
-import { Service } from 'egg';
-import * as _ from 'lodash'
+import { Service } from "egg";
+import * as _ from "lodash";
 /**
  * 书籍
  */
 export default class Book extends Service {
-  public commonOpt:any= {method:'GET',dataType:'json'}
+  public commonOpt: any = { method: "GET", dataType: "json" };
   /** 请求书城主页 */
   public async fetchHome() {
-    const {ctx,app} = this
-    const {xmBook} =app.config
+    const { ctx, app } = this;
+    const { xmBook } = app.config;
     try {
-      const res = await ctx.curl(`${xmBook.prefix}/hs/v3/channel/418`,this.commonOpt)
-      const [banner,hot,recommend,boy,girl,free,topic] = res.data.items
+      const res = await ctx.curl(
+        `${xmBook.prefix}/hs/v3/channel/418`,
+        this.commonOpt
+      );
+      const [banner, hot, recommend, boy, girl, free, topic] = res.data.items;
       return {
         banner: this._homeBanner(banner),
         hot: this._homeListVod(hot),
@@ -20,18 +23,23 @@ export default class Book extends Service {
         girl: this._homeListVod(girl),
         free: this._homeFree(free),
         topic: this._homeTopic(topic)
-      }
+      };
     } catch (error) {
       console.log(error);
     }
   }
   /** 瀑布流 */
-  public async fetchFalls(start:number,count:number){
-    const {ctx,app} = this
-    const {xmBook} = app.config
-    const opt = Object.assign(this.commonOpt,{headers:{'Cookie':xmBook.cookie}})
+  public async fetchFalls(start: number, count: number) {
+    const { ctx, app } = this;
+    const { xmBook } = app.config;
+    const opt = Object.assign(this.commonOpt, {
+      headers: { Cookie: xmBook.cookie }
+    });
     try {
-      const res= await ctx.curl(`${xmBook.prefix}/rock/book/recommend?start=${start}&count=${count}`,opt)
+      const res = await ctx.curl(
+        `${xmBook.prefix}/rock/book/recommend?start=${start}&count=${count}`,
+        opt
+      );
       return this._vodData(res.data.items);
     } catch (error) {
       console.log(error);
@@ -39,41 +47,60 @@ export default class Book extends Service {
   }
 
   /** 代理 */
-  public async bookProxy(method:'GET'|'POST' = 'GET',url:string,data?:any){
-    const {ctx,app} = this
-    const {xmBook} = app.config
-    let opt:any ={
+  public async bookProxy(
+    method: "GET" | "POST" = "GET",
+    url: string,
+    data?: any
+  ) {
+    const { ctx, app } = this;
+    const { xmBook } = app.config;
+    let opt: any = {
       method,
-      headers:{'Cookie':xmBook.cookie},
-      dataType:'json'
-    }
-    if(method === 'POST' && data){
-      opt.data = data
+      headers: { Cookie: xmBook.cookie },
+      dataType: "json"
+    };
+    if (method === "POST" && data) {
+      opt.data = data;
     }
     try {
-      const res= await ctx.curl(`${xmBook.prefix}${url}`,opt)
-      let dataRes:any = null
-      // banner
-      if(url.includes('/store/v0/fiction/list/')){
-        dataRes = this._bannerHandle(res.data)
-      }else if(url.includes('/hs/v3/channel/')){
-        dataRes = this._channelHandle(res.data)
-      }else if(url.includes("/hs/v0/android/store/category")){
-        dataRes = this._categoryHandle(res.data)
-      }else if(url.includes("/store/v0/ad/ranks")){
-        dataRes = this._rankHandle(res.data)
-      }else if(url.includes("/store/v0/ad/persistent")){
-        dataRes = this._recomendHandle(res.data)
-      }else if(url.includes("/store/v0/fiction/category/") || url.includes("/store/v0/fiction/rank")){
-        dataRes = this._vodData(res.data.items)
+      const res = await ctx.curl(`${xmBook.prefix}${url}`, opt);
+      let dataRes: any = null;
+      // home page
+      if (url === "/hs/v3/channel/418") {
+        const [banner, hot, recommend, girl, boy, free, topic] = res.data.items;
+        dataRes = {
+          banner: this._homeBanner(banner),
+          hot: this._homeListVod(hot),
+          recommend: this._homeRecommend(recommend),
+          boy: this._homeListVod(boy),
+          girl: this._homeListVod(girl),
+          free: this._homeFree(free),
+          topic: this._homeTopic(topic)
+        };
+        return dataRes;
+      }
+      // base page
+      if (url.includes("/store/v0/fiction/list/")) {
+        dataRes = this._bannerHandle(res.data);
+      } else if (url.includes("/hs/v3/channel/")) {
+        dataRes = this._channelHandle(res.data);
+      } else if (url.includes("/hs/v0/android/store/category")) {
+        dataRes = this._categoryHandle(res.data);
+      } else if (url.includes("/store/v0/ad/ranks")) {
+        dataRes = this._rankHandle(res.data);
+      } else if (url.includes("/store/v0/ad/persistent")) {
+        dataRes = this._recomendHandle(res.data);
+      } else if (
+        url.includes("/store/v0/fiction/category/") ||
+        url.includes("/store/v0/fiction/rank")
+      ) {
+        dataRes = this._vodData(res.data.items);
       }
       return dataRes;
     } catch (error) {
       console.log(error);
     }
   }
-
-
 
   /** 主页处理函数 */
   private _vodData(data) {
@@ -82,19 +109,19 @@ export default class Book extends Service {
         id: o.fiction_id,
         cover: o.cover,
         title: o.title,
-        authors:o.authors,
-        categories:o.categories,
-        chapterCount:o.chapter_count,
-        click:o.click,
-        finish:o.finish,
-        latestCreated:o.latest_created,
-        tags:o.new_tags,
-        rights:o.rights,
-        score:o.score,
-        summary:o.summary,
-        wordCount:o.word_count
-      }
-    })
+        authors: o.authors,
+        categories: o.categories,
+        chapterCount: o.chapter_count,
+        click: o.click,
+        finish: o.finish,
+        latestCreated: o.latest_created,
+        tags: o.new_tags,
+        rights: o.rights,
+        score: o.score,
+        summary: o.summary,
+        wordCount: o.word_count
+      };
+    });
   }
   private _homeBanner({ data: { data } }) {
     return _.chain(data)
@@ -103,21 +130,21 @@ export default class Book extends Service {
         return {
           img: o.ad_pic_url,
           referenceId: o.reference_id
-        }
-      })
+        };
+      });
   }
   private _homeListVod(item) {
     return {
       list: this._vodData(item.data.data),
       referenceId: item.reference_id
-    }
+    };
   }
   private _homeRecommend(item) {
-    const list = this._vodData(item.data.data)
+    const list = this._vodData(item.data.data);
     return {
       list: _.chunk(list, 15),
       referenceId: item.reference_id
-    }
+    };
   }
   private _homeFree(item) {
     const list = item.data.data.map((o: any) => {
@@ -125,96 +152,103 @@ export default class Book extends Service {
         id: o.data.fiction_id,
         cover: o.data.book_cover,
         title: o.ad_name
-      }
-    })
+      };
+    });
     return {
       list,
       referenceId: item.reference_id
-    }
+    };
   }
   private _homeTopic(item) {
     const list = item.data.data.map((o: any) => {
       return {
         img: o.ad_pic_url,
         referenceId: o.reference_id
-      }
-    })
+      };
+    });
     return {
       list,
       referenceId: item.reference_id
-    }
+    };
   }
   /** banner 处理 */
-  private _bannerHandle(data){
-    const {banner,description,items} = data
+  private _bannerHandle(data) {
+    const { banner, description, items } = data;
     return {
       banner,
       description,
-      list:this._vodData(items)
-    }
+      list: this._vodData(items)
+    };
   }
   /** 频道处理 */
-  private _channelHandle(data){
-    const {ad_setting_name,items} = data
+  private _channelHandle(data) {
+    const { ad_setting_name, items } = data;
     const group = items.map(o => {
-      const {reference_id,ad_name,data} = o
+      const { reference_id, ad_name, data } = o;
       return {
-        referenceId:reference_id,
-        name:ad_name,
-        list:this._vodData(data.data)
-      }
-    })
+        referenceId: reference_id,
+        name: ad_name,
+        list: this._vodData(data.data)
+      };
+    });
     return {
-      title:ad_setting_name,
-      list:group
-    }
+      title: ad_setting_name,
+      list: group
+    };
   }
   /** 分类 */
-  private _categoryHandle(data){
-    const {male,female} = data
+  private _categoryHandle(data) {
+    const { male, female } = data;
     const mapList = items => {
       return items.map(o => {
-        const {category_id,children,fiction_count,label,new_image,new_image2} = o
-        return {
-          id:category_id,
-          name:label,
+        const {
+          category_id,
           children,
-          count:fiction_count,
-          cover1:new_image,
-          cover2:new_image2
-        }
-      })
-    }
+          fiction_count,
+          label,
+          new_image,
+          new_image2
+        } = o;
+        return {
+          id: category_id,
+          name: label,
+          children,
+          count: fiction_count,
+          cover1: new_image,
+          cover2: new_image2
+        };
+      });
+    };
     return {
-      male:mapList(male),
-      female:mapList(female)
-    }
+      male: mapList(male),
+      female: mapList(female)
+    };
   }
   /** 排行 */
-  private _rankHandle(data){
-    const {items} = data
-    const group = items.slice(18,items.length)
+  private _rankHandle(data) {
+    const { items } = data;
+    const group = items.slice(18, items.length);
     return group.map(o => {
-      const {cover,description,name,ranks} = o
+      const { cover, description, name, ranks } = o;
       return {
-        id:ranks,
+        id: ranks,
         cover,
         name,
-        description:description.split('\n')
-      }
-    })
+        description: description.split("\n")
+      };
+    });
   }
   /** 专题 请求 */
-  private _recomendHandle(data){
-    const {items} = data
+  private _recomendHandle(data) {
+    const { items } = data;
     return items.map(o => {
-      const {ad_copy,ad_name,ad_pic_url,reference_id} = o
+      const { ad_copy, ad_name, ad_pic_url, reference_id } = o;
       return {
-        id:reference_id,
-        name:ad_name,
-        cover:ad_pic_url,
-        description:ad_copy
-      }
-    })
+        id: reference_id,
+        name: ad_name,
+        cover: ad_pic_url,
+        description: ad_copy
+      };
+    });
   }
 }
--- config/config.default.ts
+++ config/config.default.ts
@@ -1,59 +1,57 @@
-import { EggAppConfig, EggAppInfo, PowerPartial } from 'egg';
-import cacheMid from '../app/middleware/cache'
+import { EggAppConfig, EggAppInfo, PowerPartial } from "egg";
+import cacheMid from "../app/middleware/cache";
 export default (appInfo: EggAppInfo) => {
   const config = {} as PowerPartial<EggAppConfig>;
 
   // override config from framework / plugin
   // use for cookie sign key, should change to your own and keep security
-  config.keys = appInfo.name + '_1547024757636_1700';
+  config.keys = appInfo.name + "_1547024757636_1700";
 
   // add your egg config in here
   config.middleware = [];
 
   //csrf
-  config.security={
+  config.security = {
     csrf: {
       enable: false,
       ignoreJSON: true
     }
-  }
+  };
   //mongodb
   config.mongoose = {
     url: "mongodb://localhost:27017/eggapi",
     options: {}
-  }
+  };
   //redis
-  config.redis={
+  config.redis = {
     client: {
-      port: 6379,          
-      host: '127.0.0.1',
-      password:'auth',
+      port: 6379,
+      host: "127.0.0.1",
+      password: "auth",
       db: 0
     }
-  }
+  };
   //内置 middlware
   config.autoMiddlware = [
     {
-      name:'@cache',
-      middlware:cacheMid,
-      opt:{}
+      name: "@cache",
+      middlware: cacheMid,
+      opt: {}
     }
-  ]
+  ];
   // add your special config in here
   const bizConfig = {
     sourceUrl: `https://github.com/eggjs/examples/tree/master/${appInfo.name}`,
-    xmBook:{
-      prefix:'http://dushu.xiaomi.com',
-      cookie:'uLocale=zh_CN; Hm_lvt_a1d10542fc664b658c3ce982b1cf4937=1489720605,1489996190,1490060009,1490144056; Hm_lpvt_a1d10542fc664b658c3ce982b1cf4937=1490144059; app_id=mi_wap; build=8888; device_id=D950FH275FLEFLJX; user_type=2; device_hash=76dc2d8d805f607f7e10f8596e06730a; user_id=1236127735; token=s4xOpaiEvnQH-8h_y541nhFPb6E-Nf3j_8ZBQDTS5gYkuzGxs5cko2iW-tJ9Mvl8'
+    xmBook: {
+      prefix: "http://dushu.xiaomi.com",
+      cookie:
+        "uLocale=zh_CN; Hm_lvt_a1d10542fc664b658c3ce982b1cf4937=1489720605,1489996190,1490060009,1490144056; Hm_lpvt_a1d10542fc664b658c3ce982b1cf4937=1490144059; app_id=mi_wap; build=8888; device_id=D950FH275FLEFLJX; user_type=2; device_hash=76dc2d8d805f607f7e10f8596e06730a; user_id=1236127735; token=s4xOpaiEvnQH-8h_y541nhFPb6E-Nf3j_8ZBQDTS5gYkuzGxs5cko2iW-tJ9Mvl8"
     }
   };
-
-  
-
 
   // the return config will combines to EggAppConfig
   return {
     ...config,
-    ...bizConfig,
+    ...bizConfig
   };
 };
