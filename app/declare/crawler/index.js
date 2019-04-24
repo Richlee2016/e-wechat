d547eae7ff38bb47f2c59c5d7a52927f12946f45
Richlee2016 <345642459@qq.com>
2019-04-24 17:11:48 +0800

    修改格式

--- app/declare/crawler/index.js
+++ app/declare/crawler/index.js
@@ -3,70 +3,70 @@ const cheerio = require('cheerio')
 const _ = require('lodash')
 // 搜索爬取
 exports.bookSearch = async q => {
-    const url = `https://sou.xanbhx.com/search?siteid=23uscc&q=${encodeURIComponent(q)}`;
-    const html = await Rxios({ url });
-    const $ = cheerio.load(html);
-    const dom = $('ul li').get()
-    const href = box => {
-        const h = box('s2').find('a').attr('href')
-        return /.+\/html\/(\d+)\/(\d+)/.test(h)? [RegExp.$1,RegExp.$2] : h
+  const url = `https://sou.xanbhx.com/search?siteid=23uscc&q=${encodeURIComponent(q)}`
+  const html = await Rxios({ url })
+  const $ = cheerio.load(html)
+  const dom = $('ul li').get()
+  const href = box => {
+    const h = box('s2').find('a').attr('href')
+    return /.+\/html\/(\d+)\/(\d+)/.test(h) ? [RegExp.$1, RegExp.$2] : h
+  }
+  return dom.slice(1, dom.length).map(o => {
+    const box = s => $(o).find('.' + s)
+    return {
+      type: box('s1').text(),
+      name: box('s2').find('a').text().trim(),
+      href: href(box),
+      author: box('s4').text(),
+      newChapter: box('s3').find('a').text(),
+      newChapterHref: box('s3').find('a').attr('href'),
+      status: box('s6').text(),
+      update: box('s7').text()
     }
-    return dom.slice(1,dom.length).map(o => {
-        const box = s => $(o).find('.' + s)
-        return {
-            type:box('s1').text(),
-            name:box('s2').find('a').text().trim(),
-            href:href(box),
-            author:box('s4').text(),
-            newChapter:box('s3').find('a').text(),
-            newChapterHref:box('s3').find('a').attr('href'),
-            status:box('s6').text(),
-            update:box('s7').text(),
-        }
-    })
+  })
 }
 // 章节爬虫
-exports.bookChapter = async (type,chapter) => {
-    const html = await Rxios({ url:`https://www.x23us.la/html/${type}/${chapter}`});
-    const $ = cheerio.load(html);
-    const dom = $('.chapterlist').children().get()
-    let capList = []
-    dom.forEach(o => {
-        let res = {}
-        const href = $(o).find('a').attr('href')
-        if(o.name === 'dt'){
-            res.type = 'dt'
-            res.text = $(o).text()
-            capList.push(res) 
-        }else if(o.name === 'dd' && /(\d+)\.html/.test(href)) {
-            res.type = 'dd'
-            res.text = $(o).find('a').text()
-            res.href = $(o).find('a').attr('href').replace(/\.html/,"").trim()
-            capList.push(res) 
-        };
-    })
-    const author = $('.btitle span em').text()
-    return {
-        id:chapter,
-        name:$('.btitle span h1').text(),
-        preid:type,
-        author:/作者：([\u4e00-\u9fa5]+)/.test(author)?RegExp.$1 : author,
-        newChapter:$('.stats .fl a').text(),
-        newChapterHref:$('.stats .fl a').attr('href'),
-        status:$('.stats .fr i').eq(0).text(),
-        update:$('.stats .fr i').eq(1).text(),
-        context:$('.intro').remove('b').text(),
-        chapters:capList
-    }
+exports.bookChapter = async (type, chapter) => {
+  const html = await Rxios({ url: `https://www.x23us.la/html/${type}/${chapter}` })
+  const $ = cheerio.load(html)
+  const dom = $('.chapterlist').children().get()
+  let capList = []
+  dom.forEach(o => {
+    let res = {}
+    const href = $(o).find('a').attr('href')
+    if (o.name === 'dt') {
+      res.type = 'dt'
+      res.text = $(o).text()
+      capList.push(res)
+    } else if (o.name === 'dd' && /(\d+)\.html/.test(href)) {
+      res.type = 'dd'
+      res.text = $(o).find('a').text()
+      res.href = $(o).find('a').attr('href').replace(/\.html/, '').trim()
+      capList.push(res)
+    };
+  })
+  const author = $('.btitle span em').text()
+  return {
+    id: chapter,
+    name: $('.btitle span h1').text(),
+    preid: type,
+    author: /作者：([\u4e00-\u9fa5]+)/.test(author) ? RegExp.$1 : author,
+    newChapter: $('.stats .fl a').text(),
+    newChapterHref: $('.stats .fl a').attr('href'),
+    status: $('.stats .fr i').eq(0).text(),
+    update: $('.stats .fr i').eq(1).text(),
+    context: $('.intro').remove('b').text(),
+    chapters: capList
+  }
 }
 
 // 内容爬取
-exports.bookContext = async (type,chapter,id) => {
-    const html = await Rxios({ url:`https://www.x23us.la/html/${type}/${chapter}/${id}.html`});
-    const $ = cheerio.load(html,{ decodeEntities: false });
-    return {
-        id:id,
-        title:$('#BookCon h1').text(),
-        context:_.compact($('#content').html().split('<br>').map(o => o.trim()))
-    }
+exports.bookContext = async (type, chapter, id) => {
+  const html = await Rxios({ url: `https://www.x23us.la/html/${type}/${chapter}/${id}.html` })
+  const $ = cheerio.load(html, { decodeEntities: false })
+  return {
+    id: id,
+    title: $('#BookCon h1').text(),
+    context: _.compact($('#content').html().split('<br>').map(o => o.trim()))
+  }
 }
--- app/dto/movie.ts
+++ app/dto/movie.ts
@@ -1,8 +1,8 @@
-import * as Joi from "joi";
-import validator from "../middleware/validator";
+import * as Joi from 'joi';
+import validator from '../middleware/validator';
 // 条件筛选守卫
 export const MovieVodsDto = validator({
-  type: "query",
+  type: 'query',
   valid: Joi.object().keys({
     page: Joi.number(),
     size: Joi.number(),
@@ -16,7 +16,7 @@ export const MovieVodsDto = validator({
 
 // 搜索守卫
 export const MovieSearchDto = validator({
-  type: "body",
+  type: 'body',
   valid: Joi.object().keys({
     keyword: Joi.string().required(),
     page: Joi.number(),
